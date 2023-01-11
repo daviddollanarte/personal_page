ad7477bf876edd041c68f22dca0385d82f0190f1
daviddollanarte <daviddollanarte@gmail.com>
2023-01-11 20:18:29 -0300

    chore: creates text constants for translations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,22 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import Layout from "../components/Layout";
-
-const inter = Inter({ subsets: ["latin"] });
+import { home } from "../constants/textConstants/es-ES"
 
 export default function Home() {
-  const imgWidth = 1400;
   return (
     <>
       <Head>
         {/*eslint-disable-next-line react/no-unescaped-entities */}
-        <title>David Dollanarte</title>
+        <title>{home.name}</title>
         <meta name="description" content="David Dollanarte personal page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon1.ico" />
       </Head>
       <Layout>
-        <h1>David Dollanarte</h1>
+        <h1>{home.name}</h1>
         <br></br>
         <h2>
-          Web/UI <br></br> Developer
+          {home.role1}<br></br>{home.role2}
         </h2>
       </Layout>
     </>
